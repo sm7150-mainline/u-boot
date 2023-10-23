f6892406b75cb80cc2ad6e4bddf99645ded24a5b
Caleb Connolly <caleb.connolly@linaro.org>
2023-10-23 16:05:10 +0100

    clk/qcom: introduce debugcc
    debugcc is a tool for debugging the clock controllers on Qualcomm SoCs.
    It uses the built-in clock debugging registers to measure clock rates by
    muxing clocks to a special debug mux.
    
    This is a port of the userspace tool [1], as it is plain C and only
    depends on getopt, only minimal changes are necessary to integrate it
    into u-boot. The main addition is a substring search to filter which
    clocks are measured as this is quite useful when running on-device.
    
    The "clk debug" command can be used to invoke "debugcc". This tool uses
    the debug registers on the clock controller to measure the actual clock
    frequencies.
    
    List all core clock names:
    
      clk debug 0 -b core -l
    
    Measure the usb30_prim_master clock:
    
      clk debug 0 gcc_usb30_prim_master_clk
    
    Measure all clocks used by the SDcard controller:
    
      clk debug 0 -s sdcc2
    
    | BLOCK    | CLOCK                | MHz     | Hz            |
    +----------+----------------------+---------+---------------+
    | core     | gcc_sdcc2_apps_clk   |  202MHz |   201501200Hz |
    | core     | gcc_sdcc2_ahb_clk    |  100MHz |   100000860Hz |
    
    Imported at commit b3f6a94 ("Update README")
    
    [1]: https://github.com/linux-msm/debugcc
    
    Signed-off-by: Caleb Connolly <caleb.connolly@linaro.org>

--- drivers/clk/qcom/clock-qcom.c
+++ drivers/clk/qcom/clock-qcom.c
@@ -28,6 +28,8 @@
 
 #include "clock-qcom.h"
 
+#include "debugcc/debugcc.h"
+
 /* CBCR register fields */
 #define CBCR_BRANCH_ENABLE_BIT  BIT(0)
 #define CBCR_BRANCH_OFF_BIT     BIT(31)
@@ -350,11 +352,22 @@ static void __maybe_unused msm_dump_clks(struct udevice *dev)
 	dump_rcgs(dev);
 }
 
+static void msm_debug_clks(struct udevice *dev, int argc, char *const argv[])
+{
+	if (!IS_ENABLED(CONFIG_CLK_QCOM_DEBUG)) {
+		printf("Enable CONFIG_CLK_QCOM_DEBUG to debug GCC\n");
+		return;
+	}
+
+	qcom_debugcc_run(argc, argv);
+}
+
 static struct clk_ops msm_clk_ops = {
 	.set_rate = msm_clk_set_rate,
 	.enable = msm_clk_enable,
 #if IS_ENABLED(CONFIG_CMD_CLK)
 	.dump = msm_dump_clks,
+	.debug_clks = msm_debug_clks,
 #endif
 };
 
