bc5d20d998e64d1f2c10c1d4702a0236975d1ceb
Caleb Connolly <caleb.connolly@linaro.org>
2023-12-07 00:13:07 +0000

    mach-snapdragon: implement capsule update support
    Qualcomm boards flash U-Boot to the boot partition, implement support
    for determining which slot U-Boot is running from and finding the
    correct boot partition for that slot and configuring the appropriate DFU
    string.
    
    For now this only supports boards with SCSI/UFS storage where U-Boot is
    flashed to the boot partition, and only U-Boot itself is updated. In the
    future we may also support updating additional firmware components (tz,
    hyp, xbl) as well as having U-Boot installed to other partitions (e.g.
    as a first-stage bootloader).
    
    Signed-off-by: Caleb Connolly <caleb.connolly@linaro.org>

--- arch/arm/mach-snapdragon/board.c
+++ arch/arm/mach-snapdragon/board.c
@@ -300,6 +300,9 @@ int board_late_init(void)
 	configure_env();
 	qcom_late_init();
 
+	/* Configure the dfu_string for capsule updates */
+	qcom_configure_capsule_updates();
+
 	return 0;
 }
 
---
+++ arch/arm/mach-snapdragon/capsule_update.c
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Common initialisation for Qualcomm Snapdragon boards.
+ *
+ * Copyright (c) 2023 Linaro Ltd.
+ * Author: Caleb Connolly <caleb.connolly@linaro.org>
+ */
+
+#define pr_fmt(fmt) "CapsuleUpdate: " fmt
+
+#include <dm/device.h>
+#include <dm/uclass.h>
+#include <efi.h>
+#include <efi_loader.h>
+#include <malloc.h>
+#include <scsi.h>
+#include <part.h>
+
+#include "qcom-priv.h"
+
+struct efi_fw_image fw_images[] = {
+	{
+		.image_type_id = QUALCOMM_UBOOT_BOOT_IMAGE_GUID,
+		.fw_name = u"QUALCOMM-UBOOT",
+		.image_index = 1,
+	},
+};
+
+struct efi_capsule_update_info update_info = {
+	/* Filled in by configure_dfu_string() */
+	.dfu_string = NULL,
+	.num_images = ARRAY_SIZE(fw_images),
+	.images = fw_images,
+};
+
+struct part_slot_status {
+	u16 : 2;
+	u16 active : 1;
+	u16 : 3;
+	u16 successful : 1;
+	u16 unbootable : 1;
+	u16 tries_remaining : 4;
+};
+
+#define SLOT_STATUS(info) ((struct part_slot_status*)&info->type_flags)
+
+static int find_boot_partition(const char *partname, struct blk_desc *blk_dev, struct disk_partition *info)
+{
+	int ret;
+	int partnum;
+
+	for (partnum = 1;; partnum++) {
+		ret = part_get_info(blk_dev, partnum, info);
+		if (ret) {
+			return ret;
+		}
+		if (!strncmp(info->name, partname, strlen(partname)) && SLOT_STATUS(info)->active) {
+			log_debug("Found active %s partition!\n", partname);
+			return partnum;
+		}
+	}
+
+	return -1;
+}
+
+/**
+ * qcom_configure_capsule_updates() - Configure the DFU string for capsule updates
+ *
+ * U-Boot is flashed to the boot partition on Qualcomm boards. In most cases there
+ * are two boot partitions, boot_a and boot_b. As we don't currently support doing
+ * full A/B updates, we only support updating the currently active boot partition.
+ *
+ * So we need to find the current slot suffix and the associated boot partition.
+ * We do this by looking for the boot partition that has the 'active' flag set
+ * in the GPT partition vendor attribute bits.
+ */
+void qcom_configure_capsule_updates(void)
+{
+	struct blk_desc *desc;
+	struct disk_partition info;
+	int ret = 0, partnum = -1, devnum;
+	static char dfu_string[32] = { 0 };
+	char *partname = "boot";
+	struct udevice *dev = NULL;
+
+#ifdef CONFIG_SCSI
+	/* Scan for SCSI devices */
+	ret = scsi_scan(false);
+	if (ret) {
+		debug("Failed to scan SCSI devices: %d\n", ret);
+		return;
+	}
+#else
+	debug("Qualcomm capsule update running without SCSI support!\n");
+#endif
+
+	uclass_foreach_dev_probe(UCLASS_BLK, dev) {
+		if (device_get_uclass_id(dev) != UCLASS_BLK)
+			continue;
+
+		desc = dev_get_uclass_plat(dev);
+		if (!desc || desc->part_type == PART_TYPE_UNKNOWN)
+			continue;
+		devnum = desc->devnum;
+		partnum = find_boot_partition(partname, desc,
+					      &info);
+		if (partnum >= 0)
+			break;
+	}
+
+	if (partnum < 0) {
+		log_err("Failed to find boot partition\n");
+		return;
+	}
+
+	switch(desc->uclass_id) {
+	case UCLASS_SCSI:
+		snprintf(dfu_string, 32, "scsi %d=u-boot-bin part %d", devnum, partnum);
+		break;
+	case UCLASS_MMC:
+		snprintf(dfu_string, 32, "mmc part %d:%d=u-boot-bin", devnum, partnum);
+		break;
+	default:
+		debug("Unsupported storage uclass: %d\n", desc->uclass_id);
+		return;
+	}
+	log_debug("U-Boot boot partition is %s\n", info.name);
+	log_debug("DFU string: %s\n", dfu_string);
+
+	update_info.dfu_string = dfu_string;
+}
