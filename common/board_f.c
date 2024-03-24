055849b38b10f822b955f1ec79c8b21e67e76a57
Caleb Connolly <caleb.connolly@linaro.org>
2024-03-24 17:44:36 +0000

    common/board_init: setup early malloc during crt0
    Currently the early malloc initialisation is done partially in
    board_init_f_init_reserve(), which configures gd->malloc_base. But it
    isn't actually usable until initf_malloc() is called which doesn't
    happen until after fdtdec_setup().
    
    This causes an issue when using MULTI_DTB_FIT, as it expects a working
    malloc().
    
    The existence of initf_malloc() seems to be a legacy from when this was
    less standardised, but nowadays all it does is set gd->malloc_limit and
    malloc_ptr, both of which we could just assign directly from
    board_init_f_init_reserve().
    
    Make this change so that malloc() will work in fdtdec_setup() and
    simplify this early bootflow.
    
    Signed-off-by: Caleb Connolly <caleb.connolly@linaro.org>

--- common/board_f.c
+++ common/board_f.c
@@ -866,7 +866,6 @@ static const init_fnc_t init_sequence_f[] = {
 #ifdef CONFIG_TRACE_EARLY
 	trace_early_init,
 #endif
-	initf_malloc,
 	log_init,
 	initf_bootstage,	/* uses its own timer, so does not need DM */
 	event_init,
